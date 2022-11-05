d00b6bb970dc436aa63a66d5fbe34ed472a84392
Ram Deepak <>
2022-11-05 16:58:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return "hi"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=random.randint(2000, 9000))
